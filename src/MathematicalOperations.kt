89317cdb258aa3e23091d2ede9985ceb6bf3e552
Афанасьев Александр Антонович <gr602_afaan@ttit.local>
2023-04-19 19:06:56 +0700

    refactoring

--- src/MathematicalOperations.kt
+++ src/MathematicalOperations.kt
@@ -1,11 +1,19 @@
 object MathematicalOperations : Operations {
 
-    override fun multiplication(num1 : Double, num2: Double): Double = num1 * num2
+    override fun multiplication(num1 : Double, num2: Double): Double {
+        TODO()
+    }
 
-    override fun subtraction(num1: Double, num2: Double): Double = num1 - num2
+    override fun subtraction(num1: Double, num2: Double): Double{
+        TODO()
+    }
 
-    override fun division(num1: Double, num2: Double): Double = num1 / num2
+    override fun division(num1: Double, num2: Double): Double {
+        TODO()
+    }
 
-    override fun sum(num1: Double, num2: Double): Double = num2 + num2
+    override fun sum(num1: Double, num2: Double): Double {
+        TODO()
+    }
 
 }
