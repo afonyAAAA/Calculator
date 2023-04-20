a19735be40619dc6fe083e941cbb3124ee6ae663
Афанасьев Александр Антонович <gr602_afaan@ttit.local>
2023-04-20 15:50:12 +0700

    deleted workspace.xml

---
+++ src/MathematicalOperations.kt
@@ -0,0 +1,16 @@
+object MathematicalOperations : Operations {
+
+    override fun multiplication(num1 : Double, num2: Double): Double {
+        TODO()
+    }
+
+    override fun division(num1: Double, num2: Double): Double = num1 / num2
+
+    override fun sum(num1: Double, num2: Double): Double {
+        TODO()
+    }
+
+    override fun subtraction(num1: Double, num2: Double): Double {
+        TODO()
+    }
+}
